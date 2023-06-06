3ba2cccbe2508a1199e35420d179174f2f2cd84e
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-06-06 23:47:55 +0630

    feat: add resource

---
+++ resources/js/Pages/Products/Index.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    products: Array,
+})
+</script>
+<template>
+    <div>
+        <ul>
+            <li
+                v-for="product in products"
+                :key="product.id"
+            >
+                {{ product.name }} - ${{ product.price }}
+            </li>
+        </ul>
+    </div>
+</template>
